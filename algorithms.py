1d03c52e35f420af7bc34b1a3c063a0638f65cda
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-03-15 03:36:46 -0300

    Add initial algorithms and specimens

---
+++ algorithms.py
@@ -0,0 +1,95 @@
+import random
+
+from specimens import (
+    SimpleSpecimen,
+    Specimen,
+)
+
+
+class GeneticAlgorithm:
+
+    specimen = Specimen
+
+    population_size = 0.0
+    generations = 0.0
+    mutation_probability = 0.0
+
+    def __init__(self):
+        self.population = [self.specimen() for _ in range(self.population_size)]
+
+    def generate_population(self):
+        for specimen in self.population:
+            specimen.generate()
+
+    def calculate_fitness(self):
+        for specimen in self.population:
+            specimen.calculate_fitness()
+
+    def selection(self):
+        raise NotImplementedError()
+
+    def crossover(self, selected):
+        raise NotImplementedError()
+
+    def mutation(self):
+        raise NotImplementedError()
+
+    def process_generation(self, generation):
+        raise NotImplementedError()
+
+    def run(self):
+        self.generate_population()
+        self.calculate_fitness()
+
+        for generation in range(self.generations):
+            self.process_generation(generation)
+
+
+class GeneticOutputMixin:
+
+    def output_population(self, generation=None):
+        if generation is not None:
+            print('\nGeneration {}\n'.format(generation))
+        for i, specimen in enumerate(self.population):
+            print('Specimen {0}. {1}'.format(i, specimen))
+
+
+class SingleSelectionGeneticAlgorithm(GeneticAlgorithm):
+
+    def crossover(self, selected):
+        for specimen in self.population:
+            specimen.crossover(selected)
+
+    def mutation(self, selected):
+        raise NotImplementedError
+
+    def process_generation(self, generation):
+        selected = self.selection()
+        self.crossover(selected)
+        self.mutation(selected)
+        self.calculate_fitness()
+
+
+class SimpleGeneticAlgorithm(GeneticOutputMixin,
+                             SingleSelectionGeneticAlgorithm):
+
+    specimen = SimpleSpecimen
+
+    population_size = 10
+    generations = 10
+    mutation_probability = 0.2
+
+    def selection(self):
+        return min(self.population)
+
+    def mutation(self, selected):
+        """Gauss mutation with elitism"""
+        for specimen in self.population:
+            if specimen == selected:
+                continue
+            if random.random() < self.mutation_probability:
+                specimen.mutate()
+
+    def process_generation(self, generation):
+        super().process_generation(generation)
+        self.output_population(generation)
---
+++ specimens.py
@@ -0,0 +1,52 @@
+import random
+
+
+class Specimen:
+
+    def __init__(self):
+        self.fitness = 0
+
+    def generate(self):
+        raise NotImplementedError()
+
+    def calculate_fitness(self):
+        raise NotImplementedError()
+
+    def crossover(self, other):
+        raise NotImplementedError()
+
+    def mutate(self):
+        raise NotImplementedError()
+
+
+class SimpleSpecimen(Specimen):
+
+    RANGE = 20.0
+    RANGE_OFFSET = 10.0
+
+    def __str__(self):
+        return '{0:.3f} {1:.3f} {2:.3f}'.format(self.x, self.y, self.fitness)
+
+    def __lt__(self, other):
+        return self.fitness < other.fitness
+
+    def generate(self):
+        self.x = (random.random() * self.RANGE) - self.RANGE_OFFSET
+        self.y = (random.random() * self.RANGE) - self.RANGE_OFFSET
+
+    def calculate_fitness(self):
+        self.fitness = pow(self.x, 2) + pow(self.y, 2)
+
+    def crossover(self, other):
+        self.x = (self.x + other.x) / 2
+        self.y = (self.y + other.y) / 2
+
+    def mutate(self):
+        self.x = self.x * self.gaussian_multiplier()
+        self.y = self.y * self.gaussian_multiplier()
+
+    def gaussian_multiplier(self):
+        q = random.random()
+        q = (q * 2.0) - 1.0
+        q = q / 4.0
+        return 1.0 + q
