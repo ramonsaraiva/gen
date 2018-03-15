0c7b5fe36e982cc828c8a2635406974e7926b65f
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-03-15 04:06:31 -0300

    Try mixing out algorithms with different specimens

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from algorithms import (
     RouletteSelectionGeneticAlgorithm,
 )
 
+from specimens import WeirdSpecimen
+
 if __name__ == '__main__':
-    SimpleGeneticAlgorithm().run()
-    RouletteSelectionGeneticAlgorithm().run()
+    simple = SimpleGeneticAlgorithm()
+    simple_and_weird = SimpleGeneticAlgorithm(specimen=WeirdSpecimen)
+    roulette = RouletteSelectionGeneticAlgorithm()
+
+    simple.run()
+    simple_and_weird.run()
+    roulette.run()
