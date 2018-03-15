a3e664c432b7baed7bde072414182e4456f1f0be
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-03-15 19:43:53 -0300

    Fix 2D plot

--- main.py
+++ main.py
@@ -7,4 +7,3 @@ if __name__ == '__main__':
 
     roulette = RouletteSelectionGeneticAlgorithm()
     roulette.plot_2d_fitness()
-    roulette.plot_3d_fitness()
--- output.py
+++ output.py
@@ -1,6 +1,7 @@
 import random
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -13,31 +14,18 @@ class GeneticOutputMixin:
         for i, specimen in enumerate(self.population):
             print('Specimen {0}. {1}'.format(i, specimen))
 
-    def generate_fitness_population(self):
-        population = []
+    def fitness_population(self):
         for i in range(int(self.specimen.RANGE) + 1):
             v = i - self.specimen.RANGE_OFFSET
             specimen = self.specimen(v, v)
             specimen.calculate_fitness()
-            population.append(specimen)
-        return population
+            yield specimen
 
     def plot_2d_fitness(self):
-        population = self.generate_fitness_population()
+        ax, ay = [], []
+        for specimen in self.fitness_population():
+            ax.append(specimen.x)
+            ay.append(specimen.fitness)
 
-        plt.plot(
-            [specimen.x for specimen in population],
-            [specimen.fitness for specimen in population])
-        plt.show()
-
-    def plot_3d_fitness(self):
-        population = self.generate_fitness_population()
-
-        figure = plt.figure()
-        ax = plt.axes(projection='3d')
-
-        ax.plot3D(
-            [specimen.x for specimen in population],
-            [specimen.y for specimen in population],
-            [specimen.fitness for specimen in population])
+        plt.plot(ax, ay)
         plt.show()
